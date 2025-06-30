38524b04c386952dc0a82e7f5cebd6980adb4a0f
Johnny Povolny <johnny.povolny@sanity.io>
2025-06-30 14:12:49 -0700

    Remove unneeded console

--- test/utils.ts
+++ test/utils.ts
@@ -2,6 +2,3 @@ import os from "os"
 import path from "path"
 
 export const snapshotDirectory = path.join(__dirname, "snapshots", os.platform())
-
-console.log("Platform:", os.platform()) // 'darwin' for macOS
-console.log("Using directory:", snapshotDirectory)
